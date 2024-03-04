5ede6c3630e09fdb6c28f5475fff3bf3bd0e09c6
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-04 09:02:56 -0500

    diccionario de jardineria

---
+++ Main.py
@@ -0,0 +1,3 @@
+import Modulos.getClients as cliente
+
+print(cliente.getAllClientPaisRegionCiudad("Spain", "fuenlabrada", "Madrid"))
--- Modulos/getClients.py
+++ Modulos/getClients.py
@@ -1,6 +1,37 @@
-import storage import
-def searchName(palabra):
-    print(palabra)
+import Storage.cliente as cli
 
+def getAllClientesName():
+    clienteName = []
+    for i,val in cli.clientes:
+        codigoNames= dict({
+            "codigo_cliente": val.get("codigo_cliente"),
+            "nombre_cliente": val.get("nombre_cliente")
+        })
+        clienteName.append(codigoNames)
+    return clienteName
 
-searchName("busque esto plis")
+def getOneClienteCodigo(codigo):
+    for val in cli.clientes:
+        if(val.get('codigo_cliente') == codigo):
+            return{
+                "codigo_cliente": val.get('codigo_cliente'),
+                "nombre_cliente": val.get('nombre_cliente')
+            }
+        
+def getAllClientCreditCiudad(limiteCredit, Ciudad):
+    clienteCredic = list()
+    for val in cli.clientes:
+        if(val.get('limite_cliente') >= limiteCredit and val.get ('ciudad') == Ciudad):
+            clienteCredic.append(val)
+    return clienteCredic
+
+def getAllClientPaisRegionCiudad(pais, region=None,ciudad=None):
+    clientZone = list()
+    for val in cli.clientes:
+        if(
+            val.get('pais') == pais and 
+            (val.get('region') == region or val.get('region') == None) or
+            (val.get('ciudad') == ciudad or val.get('ciudad') == None)
+        ):
+            clientZone.append(val)
+    return clientZone
