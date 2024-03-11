38a77c344eb024404527618945d4dd4ba97cbbba
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-11 09:03:30 -0500

    correciones

--- Main.py
+++ Main.py
@@ -1,8 +1,4 @@
-from tabulate import tabulate
-
-import modules.getClients as cliente
-
-
+import os
 from tabulate import tabulate 
 import modules.getEmpleados as empleado
 import modules.getClients as clientes
@@ -11,76 +7,58 @@ import modules.getPedidos as pedido
 import modules.getPago as pago
 import sys
 
-if(__name__=="_name_"):
-    print(f"""
-      __  ___                    ____       _            _             __
-   /  |/  /__  ____  __  __   / __ \_____(_)___  _____(_)___  ____ _/ /
-  / /|_/ / _ \/ __ \/ / / /  / /_/ / ___/ / __ \/ ___/ / __ \/ __ `/ / 
- / /  / /  __/ / / / /_/ /  / ____/ /  / / / / / /__/ / /_/ / /_/ / /  
-/_/  /_/\___/_/ /_/\__,_/  /_/   /_/  /_/_/ /_/\___/_/ .___/\__,_/_/   
-                                                    /_/          
-                         1.cliente
-                         2.oficina
-                         3.empleado
-                         4.pedidos
-   """)
-    opcion = int(input("\nseleccione una de las opciones"))
-    if(opcion==1):
-        cliente.menu()
-    elif():
-
-#print(tabulate(cliente.getAllClientPaisRegionCiudad("Spain", "fuenlabrada", "Madrid"), tablefmt = 'grid'))
-   #Todos los nombres de los empleados
-#print(tabulate(empleado.getAllEmpleadosName()), tablefmt="grid")
-
-   #Filtrar la informacion por el codigo de jefe correspondiente
-#print(tabulate(empleado.getAllEmpleadosCode(15)))
-
-   #La informacion del nombre de la persona 
-#print(tabulate(empleado.getOneEmpleadoNombreApellidos("Ruben")tablefmt = 'grid'))
-
-   #Con el codigo del empleado y opcional el nombre muestra la informacion
-#print(tabulate(empleado.getOneEmpleadoCodeNombre(17)tablefmt = 'grid'))
-
-   #Filtrar la informacion por el codigo de jefe correspondiente
-#print(tabulate(empleado.getAllEmpleadosCode(15)))
-
-    #
-#print(tabulate(empleado.getOneEmpleadoExtension("Representante Ventas")))
 
 #PUNTOS PAGINA
 
    #1 punto
-#print(tabulate(oficina.getCodigoOfiCiudadName()))
+#print(tabulate(oficina.getCodigoOfiCiudadName(), tablefmt="grid"))
 
    #2 punto
-#print(tabulate(oficina.getCiudadTelefonoEspaña()))
+#print(tabulate(oficina.getCiudadTelefonoEspaña(), tablefmt="grid"))
 
    #3 punto
-#print(tabulate(empleado.getNombreApellidoEmailJefe()))
+#print(tabulate(empleado.getNombreApellidoEmailJefe(), tablefmt="grid"))
 
    #4 punto
-#print(tabulate(empleado.getAllJefesCode()))
+#print(tabulate(empleado.getAllJefesCode(), tablefmt="grid"))
 
    #5 punto
-#print(tabulate(empleado.getEmpleadosPuesto()))
+#print(tabulate(empleado.getEmpleadosPuesto(), tablefmt="grid"))
 
    #6 punto
-#print(tabulate(clientes.getNombreClientesEspaña()))
+#print(tabulate(clientes.getNombreClientesEspaña(), tablefmt="grid"))
 
    #7 punto
-#print(tabulate(pedido.getEstadoPedido()))
+#print(tabulate(pedido.getEstadoPedido(), tablefmt="grid"))
 
    #8 punto
-#print(tabulate(pago.getFechaPago()))
+#print(pago.getFechaPago())
+
+   #9 punto
+#print(tabulate(pedido.getAllPedidosEntregadosAtrasados(), tablefmt="grid"))
+
+   #10 punto
+#print(tabulate(pedido.getPedidosDosDias(), tablefmt="grid"))
+
+   #11 punto
+#print(tabulate(pedido.getRechazos2009(), tablefmt="grid"))
+
+   #12 punto
+#print(tabulate(pedido.getEntregadosEnero(), tablefmt="grid"))
+
+   #13 punto
+#print(tabulate(pago.getPagoPaypal(), tablefmt="grid"))
+
+   #14 punto 
+#print(tabulate(pago.getFormaDePago))
 
    #EJERCICIOS PRACTICA
 
    #Todos los nombres de los empleados
-#print(tabulate(empleado.getAllEmpleadosName()), tablefmt="grid")
+#print(tabulate(empleado.getAllEmpleadosName(), tablefmt="grid"))
 
    #Filtrar la informacion por el codigo de jefe correspondiente
-#print(tabulate(empleado.getAllEmpleadosCode(15)))
+#print(tabulate(empleado.getAllEmpleado(15)))
 
    #La informacion del nombre de la persona 
 #print(tabulate(empleado.getOneEmpleadoNombreApellidos("Ruben")))
@@ -93,3 +71,52 @@ if(__name__=="_name_"):
 
    #Muestra toda la información de las personas que no son representante de ventas
 #print(tabulate(empleado.getOneEmpleadoExtension("Representante Ventas")))
+
+
+          
+# for nombre, objeto in sys.modules.items():
+#     if nombre.startswith("modules"):
+#         modulo = getattr(objeto, "__name__", None)
+#         if ((modulo != "modules")):
+#           file = modulo.split("get")[-1]
+#           print(file)
+    
+
+
+if __name__ == "__main__":
+    while True:
+        print("""
+    __  ___                    ____       _            _             __
+   /  |/  /__  ____  __  __   / __ \_____(_)___  _____(_)___  ____ _/ /
+  / /|_/ / _ \/ __ \/ / / /  / /_/ / ___/ / __ \/ ___/ / __ \/ __ `/ / 
+ / /  / /  __/ / / / /_/ /  / ____/ /  / / / / / /__/ / /_/ / /_/ / /  
+/_/  /_/\___/_/ /_/\__,_/  /_/   /_/  /_/_/ /_/\___/_/ .___/\__,_/_/   
+                                                    /_/                
+
+        0. Salir
+        1. Cliente
+        2. Oficina
+        3. Empleado
+        4. Pedidos
+        5. Pago
+        """)
+
+        try:
+            option = int(input("\nSeleccione una de las opciones: "))
+        except ValueError:
+            print("Ingrese una opción correcta!")
+        else:
+            if option == 0:
+                break
+            elif option == 1:
+                clientes.menu()
+            elif option == 2:
+                oficina.menu()
+            elif option == 3:
+                empleado.menu()
+            elif option == 4:
+                pedido.menu()
+            elif option == 5:
+                pago.menu()
+
+      
--- modules/getClients.py
+++ modules/getClients.py
@@ -1,37 +1,12 @@
 import storage.cliente as cli
+from tabulate import tabulate
 
-def getAllClientesName():
-    clienteName = []
-    for i,val in cli.clientes:
-        codigoNames= dict({
-            "codigo_cliente": val.get("codigo_cliente"),
-            "nombre_cliente": val.get("nombre_cliente")
+def search():
+    ClienteName = list()
+    for val in cli.clientes:
+        codName = dict({
+            "Codigo": val.get("codigo_cliente"),
+            "Nombre": val.ger("nombre_cliente")
         })
-        clienteName.append(codigoNames)
-    return clienteName
-
-def getOneClienteCodigo(codigo):
-    for val in cli.clientes:
-        if(val.get('codigo_cliente') == codigo):
-            return{
-                "codigo_cliente": val.get('codigo_cliente'),
-                "nombre_cliente": val.get('nombre_cliente')
-            }
-        
-def getAllClientCreditCiudad(limiteCredit, Ciudad):
-    clienteCredic = list()
-    for val in cli.clientes:
-        if(val.get('limite_cliente') >= limiteCredit and val.get ('ciudad') == Ciudad):
-            clienteCredic.append(val)
-    return clienteCredic
-
-def getAllClientPaisRegionCiudad(pais, region=None,ciudad=None):
-    clientZone = list()
-    for val in cli.clientes:
-        if(
-            val.get('pais') == pais and 
-            (val.get('region') == region or val.get('region') == None) or
-            (val.get('ciudad') == ciudad or val.get('ciudad') == None)
-        ):
-            clientZone.append(val)
-    return clientZone
+        ClienteName.append(codName)
+    return ClienteName
